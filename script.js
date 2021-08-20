6415f274d7f5a872341ff2bec8b17acf570efc0e
oemuraye <oemuraye@gmail.com>
2021-08-20 10:29:12 +0100

    made modifications on sidebar

--- script.js
+++ script.js
@@ -8,4 +8,6 @@ const navbarMenu = document.querySelector('.body')
 //hamburger toggle
 hamburger.addEventListener('click', () => {
   navbarMenu.classList.toggle('activeNav')
+  // hamburger.classList.toggle('fa-backspace')
+  // hamburger.classList.toggle('fa-bars')
 })
